38985710f36b255ffe0acde75b6a79149cabff6f
aldehir <hello@aldehir.com>
2016-04-03 16:11:48 -0500

    Refactor TemperatureStream

--- web/temperaturestream.js
+++ web/temperaturestream.js
@@ -8,27 +8,19 @@ module.exports = TemperatureStream;
 function TemperatureStream(thermo) {
   Readable.call(this, { objectMode: true });
 
-  this._listening = false;
   this._thermo = thermo;
   this._updatecb = this._update.bind(this);
 }
 
 inherits(TemperatureStream, Readable);
 
-TemperatureStream.prototype._update = function(time, temp) {
-  let pushed = this.push({
-    event: 'temperature',
-    content: JSON.stringify([time, temp])
-  });
-
-  if (!pushed) {
-    this._thermo.removeListener('temperature', this._updatecb);
-    this._listening = false;
-  }
+TemperatureStream.prototype.unpipe = function() {
+  Readable.prototype.unpipe.apply(this, arguments);
+  this.pause();
 }
 
-TemperatureStream.prototype._read = function(size) {
-  if (this._listening) return;
+TemperatureStream.prototype.resume = function() {
+  Readable.prototype.resume.apply(this, arguments);
 
   // Listen for temperature changes
   this._thermo.on('temperature', this._updatecb);
@@ -38,6 +30,25 @@ TemperatureStream.prototype._read = function(size) {
     event: 'temperature-history',
     content: JSON.stringify(this._thermo.history)
   });
+}
 
-  this._listening = true;
+TemperatureStream.prototype.pause = function() {
+  Readable.prototype.pause.apply(this, arguments);
+  this._thermo.removeListener('temperature', this._updatecb);
+}
+
+
+TemperatureStream.prototype._update = function(time, temp) {
+  let pushed = this.push({
+    event: 'temperature',
+    content: JSON.stringify([time, temp])
+  });
+
+  if (!pushed) {
+    this.pause();
+  }
+}
+
+TemperatureStream.prototype._read = function() {
+  // Don't really have to do anything, since this.push is triggered by events
 }
